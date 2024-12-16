ba1a4f78a706e89c5361d65ff248bf11eba8c2d5
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2024-12-16 15:39:38 +0100

    final prueba scrapeo.

--- src/call_api_moviesdatabase.py
+++ src/call_api_moviesdatabase.py
@@ -4,60 +4,64 @@ import http.client
 import json
 import pandas as pd # type: ignore
 import asyncio
+import time
 
 import sys
 sys.path.append("../")
 from src import soporte as sop
 
-async def get_datos_movies(genre, titleType, page,key ):
+def get_datos_movies(genre, titleType, page,key ):
 
-    result = []
-    #try:
-    url = http.client.HTTPSConnection("moviesdatabase.p.rapidapi.com")
+    result_final = []
+    try:
+        url = http.client.HTTPSConnection("moviesdatabase.p.rapidapi.com")
 
-    headers = {
-        'x-rapidapi-key': key,
-        'x-rapidapi-host': "moviesdatabase.p.rapidapi.com"
-    }
+        headers = {
+            'x-rapidapi-key': key,
+            'x-rapidapi-host': "moviesdatabase.p.rapidapi.com"
+        }
 
-    url_data = f"/titles?genre={genre}&startYear=1990&titleType={titleType}&sort=year.incr&page={page}"
+        url_data = f"/titles?genre={genre}&startYear=1990&titleType={titleType}&sort=year.incr&page={page}"
 
-    url.request(f"GET", url_data, headers=headers)
+        url.request(f"GET", url_data, headers=headers)
 
-    res = url.getresponse()
-    data = res.read()
-    dicc_datos = json.loads(data.decode("utf-8"))
+        res = url.getresponse()
+        data = res.read()
+        dicc_datos = json.loads(data.decode("utf-8"))
 
-    result = dicc_datos['results']
+        result = dicc_datos['results']
 
-    for r in result:
-        print(r)
-        thisdict = {
-            "id": r['id'],
-            "titleText": r['titleText'],
-            # "releaseYear":  r['releaseYear']
-            }
-        result.append(thisdict)
-    #except:
-        #print(f"Error al hacer peticion api, en get_datos_movies: {url}")
-    return result
+        for r in result:
+            thisdict = {
+                        "id": r.get('id', "N/A"),
+                        "titleText": r.get('titleText', {}).get('text', "N/A"),
+                        "releaseYear": r.get('releaseYear', {}).get('year', "N/A"),
+                        "releaseMonth": r.get('releaseDate', {}).get('month', "N/A"),
+                        "genre": genre
+                }
+            result_final.append(thisdict)
+    except:
+        print(f"Error al hacer peticion api, en get_datos_movies: {url}")
 
-async def alamacenar_datos_in_csv(key):
+    return result_final
+
+def alamacenar_datos_in_csv(key):
 
     df_final = pd.DataFrame()
 
     # genre = ["Drama", "Comedy", "Action", "Fantasy", "Horror", "Mystery", "Romance", "Thriller"]
     # titleType = ["movie","short"]
 
-    genre = ["Drama"]
+    genre = ["Horror"]
     titleType = ["movie"]
      
-    for p in range(1,2):
-        print(p)
+    for p in range(1,3):
         for g in genre:
             for t in titleType:
-                diccionario = await get_datos_movies(g, t, p, key)
+                time.sleep(5) # Sleep for 3 seconds
+                diccionario = get_datos_movies(g, t, p, key)
                 df_temp = pd.DataFrame(diccionario)
+                break
 
             df_final = pd.concat([df_temp, df_final], ignore_index=True)
             df_final.to_csv(f"../data/data_movies.csv")
